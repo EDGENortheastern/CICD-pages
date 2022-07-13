a7e5c0bd2a3134102eb4e917b171d317f4efc971
Katia <katia.punter@storyshareplatform.com>
2022-07-13 10:46:27 +0100

    I have no idea what I am doing

--- logic.mjs
+++ logic.mjs
@@ -1,6 +1,9 @@
 const convertToNum = (val) => parseFloat(val);
 
 const calculateResults = (amount, percent) => {
+    if (percent > 100){
+        return "You cannot calculate the percentage";
+    }
     return (convertToNum(amount)*convertToNum(percent))/100;
 }
 
