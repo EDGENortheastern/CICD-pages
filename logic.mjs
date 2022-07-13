07086f5a4bed440ad5ab073efd1bf020d25c61fc
Katia <katia.punter@storyshareplatform.com>
2022-07-13 11:11:03 +0100

    Changes the coverage report

--- logic.mjs
+++ logic.mjs
@@ -1,9 +1,6 @@
 const convertToNum = (val) => parseFloat(val);
 
 const calculateResults = (amount, percent) => {
-    if (percent > 100){
-        return "You cannot calculate the percentage";
-    }
     return (convertToNum(amount)*convertToNum(percent))/100;
 }
 
